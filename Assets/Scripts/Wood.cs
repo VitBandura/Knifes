43ab0dbf69c535a4bdf335c10ee06a3b1758c796
Vit <banduravit@gmail.com>
2022-04-02 18:44:12 +0300

    Added simple sprites and rotation script for target

--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -1,6 +1,12 @@
+using System;
 using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
-    
+    [SerializeField] private float _rotationSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0,0, _rotationSpeed);
+    }
 }
