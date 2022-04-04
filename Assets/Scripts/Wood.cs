6abf95f43c17ab321b4543c85fc5861625f0ada8
Vit <banduravit@gmail.com>
2022-04-04 14:18:08 +0300

    Added target shaking feature, some collider fixes

--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -1,11 +1,44 @@
+using System.Collections;
+using Events;
+using SimpleEventBus.Disposables;
 using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
+    private const float ShakingDuration = 0.05f;
+    private readonly Vector3 _shakingOffset = new (0, 0.08f);
+    
     [SerializeField] private float _rotationSpeed;
+
+    private CompositeDisposable _subscriptions;
+    
+    private void Awake()
+    {
+        _subscriptions = new CompositeDisposable
+        {
+            EventStreams.GameEvents.Subscribe<KnifeGetsIntoTargetEvent>(ShakeTarget)
+        };
+    }
+
+    private void ShakeTarget(KnifeGetsIntoTargetEvent obj)
+    {
+        StartCoroutine(Shake());
+    }
 
     private void FixedUpdate()
     {
         transform.Rotate(Vector3.forward, _rotationSpeed);
+    }
+
+    private IEnumerator Shake()
+    {
+        transform.position += _shakingOffset;
+        yield return new WaitForSeconds(ShakingDuration);
+        transform.position -= _shakingOffset;
+    }
+
+    private void OnDestroy()
+    {
+        _subscriptions.Dispose();
     }
 }
