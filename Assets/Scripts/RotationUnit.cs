8d6d61f43af15fcc084c8f1148cb1e3e37fe94d9
Vit <banduravit@gmail.com>
2022-04-05 18:05:29 +0300

    Added customizable wood rotation mode

---
+++ Assets/Scripts/RotationUnit.cs
@@ -0,0 +1,9 @@
+    
+using System;
+
+[Serializable]
+public class RotationUnit
+{
+    public float speed;
+    public float duration;
+}
--- Assets/Scripts/WoodRotation.cs
+++ Assets/Scripts/WoodRotation.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WoodRotation : MonoBehaviour
@@ -5,13 +6,35 @@ public class WoodRotation : MonoBehaviour
     private WheelJoint2D _wheelJoint2D;
     private JointMotor2D _jointMotor2D;
 
-    [SerializeField] private float _speed;
+    [SerializeField] private RotationUnit[] _rotationModes;
 
     private void Awake()
     {
         _wheelJoint2D = GetComponent<WheelJoint2D>();
         _jointMotor2D = _wheelJoint2D.motor;
-        _jointMotor2D.motorSpeed = _speed;
-        _wheelJoint2D.motor = _jointMotor2D;
+
+        StartCoroutine(RotateInMixedMode());
+    }
+
+    private IEnumerator RotateInMixedMode()
+    {
+        var unitIndex = 0;
+        var unitIndexMax = _rotationModes.Length - 1;
+        while (true)
+        {
+            _jointMotor2D.motorSpeed = _rotationModes[unitIndex].speed;
+            _wheelJoint2D.motor = _jointMotor2D;
+
+            yield return new WaitForSeconds(_rotationModes[unitIndex].duration);
+
+            if (unitIndex < unitIndexMax)
+            {
+                unitIndex++;
+            }
+            else
+            {
+                unitIndex = 0;
+            }
+        }
     }
 }
