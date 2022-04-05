72f7fe24d908989ed05b2c48f3c4ea7cc0a19e7e
Vit <banduravit@gmail.com>
2022-04-05 19:43:07 +0300

    Added particles of knife hit

--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -1,6 +1,31 @@
+using Events;
+using SimpleEventBus.Disposables;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
     [SerializeField] private float _durability;
+    [SerializeField] private ParticleSystem _harmParticles;
+    
+    private CompositeDisposable _subscriptions;
+
+    private void Awake()
+    {
+        
+        _subscriptions = new CompositeDisposable
+        {
+            EventStreams.GameEvents.Subscribe<KnifeGetsIntoTargetEvent>(HandleKnifeHit)
+        };
+    }
+
+    private void HandleKnifeHit(KnifeGetsIntoTargetEvent obj)
+    {
+        Instantiate(_harmParticles, transform.position, quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        _subscriptions.Dispose();
+    }
 }
