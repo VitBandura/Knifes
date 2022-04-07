4514a2566428c43dcdcaccad6f85a28f6557a17c
Vit <banduravit@gmail.com>
2022-04-07 16:53:05 +0300

    Rework of wood manager

--- Assets/Scripts/WoodManager.cs
+++ Assets/Scripts/WoodManager.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class WoodManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _woodPrefab;
+    [SerializeField] private GameObject _woodRotator;
     [SerializeField] private GameObject _woodDestroyedPrefab;
-
-    private GameObject _wood;
+    
     private GameObject _woodDestroyed;
     private Vector3 _position;
 
@@ -21,27 +20,23 @@ public class WoodManager : MonoBehaviour
 
         _position = transform.position;
         
-        _wood = Instantiate(_woodPrefab);
-        _wood.transform.position = _position;
-        _wood.SetActive(false);
-        
         _woodDestroyed = Instantiate(_woodDestroyedPrefab);
         _woodDestroyed.transform.position = _position;
         _woodDestroyed.SetActive(false);
         
-        _wood.SetActive(true);
+        _woodRotator.SetActive(true);
     }
 
     private void HandleWoodDestruction(TargetDestroyedEvent obj)
     {
-        var stuckKnives = _wood.GetComponentsInChildren<Transform>();
+        var stuckKnives = _woodRotator.GetComponentsInChildren<Transform>();
         for (var i = 2; i < stuckKnives.Length; i++)
         {
             stuckKnives[i].parent = _woodDestroyed.transform;
             stuckKnives[i].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
-        _wood.SetActive(false);
-        _woodDestroyed.transform.rotation = _wood.transform.rotation;
+        _woodRotator.SetActive(false);
+        _woodDestroyed.transform.rotation = _woodRotator.transform.rotation;
         _woodDestroyed.SetActive(true);
     }
 
