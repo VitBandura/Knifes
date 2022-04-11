284c81a3386570f1a65b5afc0f783e48cd10b836
Vit <banduravit@gmail.com>
2022-04-11 14:19:22 +0300

    Added random generation of level general value in game controller

---
+++ Assets/Scripts/Events/GameStartedEvent.cs
@@ -0,0 +1,16 @@
+using SimpleEventBus.Events;
+
+namespace Events
+{
+    public class GameStartedEvent : EventBase
+    {
+        public int WoodDurability;
+        public int KnifeCountUI;
+
+        public GameStartedEvent(int woodDurability, int knifeCountUI)
+        {
+            WoodDurability = woodDurability;
+            KnifeCountUI = knifeCountUI;
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using Events;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
+    private const int MAX_LEVEL_GENERAL_VALUE = 11;
+    private const int MIN_LEVEL_GENERAL_VALUE = 5;
     
+    private int _randomLevelGeneralValue;
+    private int _woodDurability;
+    private int _knifeCountUI;
+
+    private void Awake()
+    {
+        GenerateLevelGeneralValue();
+    }
+
+    private void GenerateLevelGeneralValue()
+    {
+        _randomLevelGeneralValue = Random.Range(MIN_LEVEL_GENERAL_VALUE, MAX_LEVEL_GENERAL_VALUE);
+        _woodDurability = _randomLevelGeneralValue;
+        _knifeCountUI = _randomLevelGeneralValue;
+        
+        EventStreams.GameEvents.Publish(new GameStartedEvent(_woodDurability, _knifeCountUI));
+    }
 }
